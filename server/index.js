ed3ff8972d38874f9e58067f1409a80c1259ae64
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2023-08-14 11:05:38 +0300

    001 Backend Configurations and Middleware Setup

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* Configirations */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* File storage config */
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "public/assets");
+  },
+  filename(req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
